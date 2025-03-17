f405f43a7b228223686b952445412b77835435ef
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-18 01:15:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function insert_row() {
 	let table = document.getElementById('sampleTable');
 	let new_row = table.insertRow(0);
 	
-	let new_cell1 = new_row.insertCell(0);
-	let new_cell2 = new_row.insertCell(1);
+	let new_Cell1 = new_row.insertCell(0);
+	let new_Cell2 = new_row.insertCell(1);
 
 	
       new_cell1.innerHTML = "new_Cell1"
