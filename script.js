e84abcfbc9fbcd7a866d1992f955e093a5b0dcf9
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-18 01:29:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 function insert_row() {
-	let table = document.getElementById('sampleTable');
-	let new_row = table.insertRow(0);
-	
-	let new_Cell1 = new_row.insertCell(0);
-	let new_Cell2 = new_row.insertCell(1);
+    let table = document.getElementById('sampleTable');
+    let new_row = table.insertRow(0);
 
-	
-      new_Cell1.innerHTML = "New Cell1"
-      new_Cell2.innerHTML = "New Cell2"
-	
-}
+    let new_Cell1 = new_row.insertCell(0);
+    let new_Cell2 = new_row.insertCell(1);
 
-	
-	
+    new_Cell1.innerHTML = "New Cell1";
+    new_Cell2.innerHTML = "New Cell2";
 }
+	
+	
